7da37e4c4776d91de7569952a4f9511f1042ea62
oussama <bakkali.oussama.solicode@gmail.com>
2026-09-09 10:09:27 +0100

    Échanger les valeurs de deux variables

---
+++ Algorithme/echange.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 20;
+
+console.log("Avant l'échange :");
+console.log("a =", a);
+console.log("b =", b);
+
+let temporaire = a;
+a = b;
+b = temporaire;
+
+console.log("Après l'échange :");
+console.log("a =", a);
+console.log("b =", b);
--- Algorithme/échanger_deux_variable.js
+++
@@ -1,6 +0,0 @@
-let a = 55;
-let b = 100;
-let c = a;
-a = b;
-b = c;
-console.log(a, b);
