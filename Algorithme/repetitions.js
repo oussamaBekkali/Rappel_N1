26a47929537bf8401f45922dcc5f03df96c63b99
oussama <bakkali.oussama.solicode@gmail.com>
2026-09-11 09:27:16 +0100

    Parcourir et traiter un tableau

--- Algorithme/repetitions.js
+++ Algorithme/repetitions.js
@@ -1,13 +1,28 @@
-let nombres = [4, 7, 2, 7, 9, 4, 5, 7];
+let arr = [2, 3, 7, 0, 5, 3, 2, 1, 3, 0];
+let dup = [];
 
-let repetitions = [];
+for (let a = 0; a < arr.length; a++) {
+  let count = 0;
 
-for (let i = 0; i < nombres.length; i++) {
-  if (nombres.indexOf(nombres[i]) !== nombres.lastIndexOf(nombres[i])) {
-    if (!repetitions.includes(nombres[i])) {
-      repetitions.push(nombres[i]);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[a] === arr[i]) {
+      count++;
+    }
+  }
+
+  if (count > 1) {
+    let alreadyExist = false;
+
+    for (let j = 0; j < dup.length; j++) {
+      if (dup[j] === arr[a]) {
+        alreadyExist = true;
+      }
+    }
+
+    if (alreadyExist == false) {
+      dup[dup.length] = arr[a];
     }
   }
 }
 
-console.log(repetitions);
+console.log(dup);
