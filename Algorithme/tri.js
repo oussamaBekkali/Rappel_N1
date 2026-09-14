8d341a1f7d050f05701a639871a9da015417602d
oussama <bakkali.oussama.solicode@gmail.com>
2026-09-14 13:20:12 +0100

    Trier les éléments d’un tableau

--- Algorithme/tri.js
+++ Algorithme/tri.js
@@ -1,11 +1,13 @@
-let nbs = [2, 8, 10, 33, 9, 99];
+let nbs = [2, 8, 10, 33, 9, 99, 1];
 
 for (let a = 0; a < nbs.length; a++) {
   for (let b = 0; b < nbs.length; b++) {
-    if (nbs[b] > nbs[b + 1]) {
-      let temp = nbs[b];
-      nbs[b] = nbs[b + 1];
-      nbs[b + 1] = temp;
+    if (b !== nbs.length) {
+      if (nbs[b] > nbs[b + 1]) {
+        let temp = nbs[b];
+        nbs[b] = nbs[b + 1];
+        nbs[b + 1] = temp;
+      }
     }
   }
 }
